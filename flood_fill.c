26bb506a623726f66eb24aaf1f7f8ba1965111aa
AimadBifkirn <aimadbifkirn@gmail.com>
2025-02-24 21:54:49 +0000

    small changes

--- flood_fill.c
+++ flood_fill.c
@@ -6,24 +6,24 @@
 /*   By: abifkirn <abifkirn@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 10:58:07 by abifkirn          #+#    #+#             */
-/*   Updated: 2025/02/22 10:58:52 by abifkirn         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:40:00 by abifkirn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	flood_fill(t_general *general, char **str, int x, int y)
+void	flood_fill(t_general *general, char **str, int y, int x)
 {
-	if (x < 0 || y < 0 || x > general->lenght || y > general->width)
+	if (y < 0 || x < 0 || y > general->lenght || x > general->width)
 		return ;
-	if (str[x][y] == 'E')
-		str[x][y] = '1';
-	if (str[x][y] == '0' || str[x][y] == 'P' || str[x][y] == 'C')
+	if (str[y][x] == 'E')
+		str[y][x] = '1';
+	if (str[y][x] == '0' || str[y][x] == 'P' || str[y][x] == 'C')
 	{
-		str[x][y] = 'F';
-		flood_fill(general, str, x + 1, y);
-		flood_fill(general, str, x - 1, y);
-		flood_fill(general, str, x, y + 1);
-		flood_fill(general, str, x, y - 1);
+		str[y][x] = 'F';
+		flood_fill(general, str, y + 1, x);
+		flood_fill(general, str, y - 1, x);
+		flood_fill(general, str, y, x + 1);
+		flood_fill(general, str, y, x - 1);
 	}
 }
--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: abifkirn <abifkirn@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 10:38:49 by abifkirn          #+#    #+#             */
-/*   Updated: 2025/02/24 21:32:30 by abifkirn         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:36:08 by abifkirn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,6 @@ int	main(int argc, char **argv)
 	flood_fill(general, general->copy_map, general->y, general->x);
 	if (check_result(general->copy_map))
 		return (print_error("Error\ncan't play in this map\n", &general));
-	printf ("%d\t%d\n", general->x, general->y);
 	window_work(&general);
 	free_struct(&general);
 	return (0);
